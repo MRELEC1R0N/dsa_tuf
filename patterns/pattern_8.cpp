0360d3c414d71c44cb1137b34c50c98e04937321
MRELEC1R0N <piyushsingh5629@gmail.com>
2024-04-06 19:22:08 +0530

    10 questions completed

--- patterns/pattern_8.cpp
+++ patterns/pattern_8.cpp
@@ -5,19 +5,15 @@ int main(){
     int n;
     cin>>n;
     for(int i = 0; i < n; i++){
-        for(int j = 0 ; j < n-i-1 ; j++){
+        for(int j = 0 ; j < i ; j++){
             cout << " ";
-        }        
+        }
         
-        for(int j = 0 ; j < 2*i+1 ; j++){
+        for(int j = 0 ; j < 2*n-2*i-1 ; j++){
             cout << "*";
         }
 
-        for(int j = 0 ; j < n-i-1 ; j++){
-            cout << " ";
-        }  
-
-
+        
         cout << endl;
         
     }
